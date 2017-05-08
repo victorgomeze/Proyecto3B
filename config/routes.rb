3a99f9533e0be9ceda2ab79c4b807f3f0b5d8e27
FbnCancino <fabian.ecr92@gmail.com>
2017-05-08 19:32:10 -0300

    arreglo archivos routes.rb

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+<<<<<<< HEAD
+  resources :boleta_venta
+  resources :registros
+  resources :trabajadors
+=======
+  resources :productos
+  resources :carro_compras
+  resources :carro_ventes
+  resources :boleta_compras
+  resources :trabajadors
+  resources :registros
+  resources :proveedors
+  resources :clientes
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
