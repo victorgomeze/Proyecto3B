75cff68581157730ba21c933b0b0ed18dd9a6d07
victorgomeze <gs_kototo@hotmail.com>
2017-07-05 17:42:28 -0400

    Arreglo vistas

--- app/models/registro.rb
+++ app/models/registro.rb
@@ -5,23 +5,6 @@ class Registro < ApplicationRecord
 validates :fecha, presence: { message: "fecha no debe estar vacio"}, confirmation: true
 #validacion fecha 
 
-#validacion hora_ingreso no null
-validates :hora_ingreso, presence: { message: "hora ingreso no debe estar vacio"}, confirmation: true
-#validacion hora_ingreso numeros
-validates :hora_ingreso, format: { with: /\A[0-9]+\z/, message: "Solo se aceptan numeros" }
-#validacion hora_ingreso minimo 1 digito
-validates :hora_ingreso, length: { minimum: 1 }
-#validacion hora_ingreso maximo 2 digito
-validates :hora_ingreso, length: { maximum: 2 }
 
-
-#validacion hora_salida no null
-validates :hora_salida, presence: { message: "hora salida no debe estar vacio"}, confirmation: true
-#validacion hora_salida numeros
-validates :hora_salida, format: { with: /\A[0-9]+\z/, message: "Solo se aceptan numeros" }
-#validacion hora_salida minimo 1 digito
-validates :hora_salida, length: { minimum: 1 }
-#validacion hora_salida maximo 2 digito
-validates :hora_salida, length: { maximum: 2 }
 
 end
